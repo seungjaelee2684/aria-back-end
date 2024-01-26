ce0f51c5f6484d23a09c516c3b6124893eafa9f5
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-01-26 11:03:08 +0900

    <feat: express-session 설정 및 멘토 api 구축>

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const session = require('express-session');
 const main = require('./Router/mainRouter');
 const mentor = require('./Router/mentorRouter');
 const notice = require('./Router/noticeRouter');
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // 허용할 출처
+    origin: true, // 허용할 출처
+    credentials: true,
     optionsSuccessStatus: 200, // CORS 요청 성공 상태 코드
 };
+
+app.use(
+    session({
+        resave: false, // 세션이 변경되지 않은 경우에도 다시 저장할지
+        saveUninitialized: true, // 초기화되지 않은 세션 저장할지
+        secret: "Aria", // 세션 암호화 키
+        cookie: {
+            httpOnly: true, // JavaScript에서 쿠키 접근을 방지할지
+            secure: true // 보안 연결(HTTPS)을 사용하지 않을 시 세션 쿠키 전송을 막을지
+        }
+    })
+);
 
 app.use(cors(corsOptions));
 
