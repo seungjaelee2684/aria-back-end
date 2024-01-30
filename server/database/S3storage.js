9f9c087a29695b36f9fb3e2aaabeb372080b2585
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-01-31 01:16:09 +0900

    <update: 쿼리문 수정>

--- server/database/S3storage.js
+++ server/database/S3storage.js
@@ -1,21 +1,22 @@
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
 
-aws.config.update({
+AWS.config.update({
     region: 'ap-northeast-2',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-const s3 = new aws.S3()
+const s3 = new AWS.S3()
 
 const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp', '.webp'];
 
 const imageUploader = multer({
     storage: multerS3({
         s3: s3,
+        acl: 'publick-read-write',
         bucket: 'ariaacademy',
         key: (req, file, callback) => {
             const uploadDirectory = req.query.directory ?? '';
@@ -26,7 +27,6 @@ const imageUploader = multer({
             }
             callback(null, `${uploadDirectory}/${uniqueIdentifier}_${file.originalname}`)
         },
-        acl: 'publick-read-write'
     })
 });
 
