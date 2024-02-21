1f6fc6e556e0d89a856573c47888dc42c003478e
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-02-21 11:35:09 +0900

    <update: 업데이트 정보 api수정>

--- server/database/S3storage.js
+++ server/database/S3storage.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-require('dotenv').config()
+require('dotenv').config();
 const path = require('path');
 const { S3Client, HeadObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 
